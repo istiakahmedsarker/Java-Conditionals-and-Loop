7cbf8e5c729eed6a1e649fa4ee023fc0d6518d90
Istiak AHmed <gamerestiak99@gmail.com>
2023-12-30 20:51:17 +0600

    solved the question AverageOfNNumbers updated it

--- ++Assignment+++/AverageOfNNumbers.java
+++ ++Assignment+++/AverageOfNNumbers.java
@@ -4,26 +4,31 @@ import java.util.Scanner;
 public class AverageOfNNumbers {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Tell how many numbers you want to input");
+        System.out.println("Input the numbers and input x to quit");
         ArrayList <Integer>  givenNumbers = new ArrayList<>();
-        int userInput = in.nextInt();
+        String userInput = in.next();
         int avg = 0;
 
         while(true){
-
-            String input = in.next();
-            if(input.charAt(0) == 'x' || input.charAt(0) == 'X' ){
+            if(userInput.charAt(0) == 'x' || userInput.charAt(0) == 'X' ){
                 break;
             }
             else{
-                givenNumbers.add(userInput);
+                int integerUserInput = Integer.parseInt(userInput);
+                givenNumbers.add(integerUserInput);
                 for(int i=0;i< givenNumbers.size();i++){
                     int singleInt = givenNumbers.get(i);
-                    avg = avg + singleInt;
+//                    avg = avg + singleInt;
+                    System.out.println(singleInt);
                 }
-                avg  /= givenNumbers.size();
+
             }
+            userInput = in.next();
         }
+//        avg  = avg / givenNumbers.size();
+//        System.out.println(avg);
         System.out.println(avg);
+//        System.out.println(givenNumbers);
+        System.out.println(givenNumbers.size());
     }
 }
